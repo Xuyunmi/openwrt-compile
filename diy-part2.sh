17d23b02dfece0f4d357b848de2cbb511e63870e
bigDshe <105616859+bigDshe@users.noreply.github.com>
2024-02-02 21:36:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,8 +26,8 @@ sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' openwrt/target/linux/bcm53xx/Ma
 #更换banner
 cp -rf banner openwrt/package/base-files/files/etc/banner
 #更换主题背景
-cp -rf extra/bg1.jpg openwrt/feeds/kenzo/luci-theme-argonne/htdocs/luci-static/argonne/img/bg1.jpg
-rm -rf openwrt/feeds/kenzo/luci-theme-argonne/README.md
+cp -rf extra/bg1.jpg openwrt/feeds/kenzo/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
+rm -rf openwrt/feeds/kenzo/luci-theme-argone/README.md
 cp -rf extra/main_bg.jpg openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg.jpg
 cp -rf extra/main_bg_5k.jpg openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
 #更换主题neobird快捷应用栏
